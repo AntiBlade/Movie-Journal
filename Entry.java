9d7bf35ecacbcb62908e04cca2ccada3490a3964
Christopher Phifer <cphifer97@gmail.com>
2015-10-23 23:01:26 -0400

    Guilt commit

--- Entry.java
+++ Entry.java
@@ -7,5 +7,22 @@
 
 public class Entry {
     private Media item;
-    private 
+    private double userRating;
+    private String userReview;
+    private boolean hasSeen;
+    private String entryDescription;
+    private int[] location = new int[2];
+    // TODO: Add more features
+
+    public void setItem(Media newItem) {item = newItem;}
+    public Media getItem() {return item;}
+    public void setUserRating(double newRating) {userRating = newRating;}
+    public double getUserRating() {return userRating;}
+    public void setUserReview(String newReview) {userReview = newReview;}
+    public String getUserReview() {return userReview;}
+    public void setHasSeen(boolean seen) {hasSeen = seen;}
+    public boolean getHasSeen() {return hasSeen;}
+    public void setEntryDescription(String newDescription) {entryDescription = newDescription;}
+    public String getEntryDescription() {return entryDescription;}
+    
 }
