7e2f55bdd8451377b95fa446b355ff0dfe41f8a3
Christopher Phifer <cphifer97@gmail.com>
2015-10-23 22:19:58 -0400

    Made JSONObject folder and created Entry.java

---
+++ Entry.java
@@ -0,0 +1,11 @@
+/*
+ * Entry.java
+ * Purpose: Contains a Media and user-entered values
+ *      By: Caleb Lucas-Foley, Harrison Keiser, Chris Phifer
+ *      On: October 23, 2015
+ */
+
+public class Entry {
+    private Media item;
+    private 
+}
