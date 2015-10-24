b7bdef6bd8c8fd6bebc5226cdcf3c9fdd044338b
Harrison <uwaces@gmail.com>
2015-10-24 02:09:50 -0400

    Create UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 
+ * @author Harrison Kaiser
+ * @version 0.0.1
+ */
+
+public class UserInput
+{
+    public static int main(String[]  args){
+        System.out.print("Instructions");
+    }
+}
