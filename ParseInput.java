32cfe90e52aeac4b607d38fbd74e6630ba76993b
Iguanotron <clucasfoley@gmail.com>
2015-10-24 03:58:57 -0400

    fixed some spacing

--- ParseInput.java
+++ ParseInput.java
@@ -13,19 +13,20 @@ public class ParseInput
    ParseInput(Journal cur){
        current = cur;
    }
+
     public boolean parse(String input, Scanner console){
-       String[] words = input.split("\\S");
-       boolean ret = true;
-       switch(words[0].toLowerCase()){
+        String[] words = input.split("\\S");
+        boolean done = false;
+        switch(words[0].toLowerCase()){
         case "search":
-           String search;
+            String search;
             if(words[1].equals("online")){
                 search = reConcat(words, " ", 2);
                 searchDB(search);
             }else if(words[1].equals("local")){
                 search = reConcat(words, " ", 2);
             } else{
-                 search = reConcat(words, " ", 1);
+                search = reConcat(words, " ", 1);
             }
             
         break;
@@ -38,22 +39,22 @@ public class ParseInput
         case "remove":
         
         break;
-       case "quit":
-           ret = false;
-       break;
-       case "load":
-           //load new journal from filename words[1]
-       break;
+        case "quit":
+            done = true;
+        break;
+        case "load":
+            //load new journal from filename words[1]
+        break;
+        }
+        return done;
     }
-    return ret;
-   }
-   private String reConcat(String[] a, String s, int first){
-       String out = "";
-       for(int i = first; i < a.length; i++){
-           out += a[i] + s;
-       }
-       return out;
-   }
+    private String reConcat(String[] a, String s, int first){
+        String out = "";
+        for(int i = first; i < a.length; i++){
+            out += a[i] + s;
+        }
+        return out;
+    }
       private String reConcat(String[] a){
        return reConcat(a, " ", 0);
    }
