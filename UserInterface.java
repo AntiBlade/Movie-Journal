9ea01e5a21556ca29416b440de40ddd33077d362
Christopher Phifer <cphifer97@gmail.com>
2015-10-23 23:37:27 -0400

    Made skeleton for UserInterface

---
+++ UserInterface.java
@@ -0,0 +1,9 @@
+/*
+ * UserInterface.java
+ * Purpose: Implement the user interface for the movie journal
+ *      By: Caleb Lucas-Foley, Harrison Keiser, Chris Phifer
+ */
+
+public class UserInterface {
+
+}
