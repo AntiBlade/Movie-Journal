f5ef8f2b12fbb199c8acd79f932022f0d2908b41
Christopher Phifer <cphifer97@gmail.com>
2015-10-23 23:19:05 -0400

    Wrote Entry class

--- Entry.java
+++ Entry.java
@@ -14,15 +14,99 @@ public class Entry {
     private int[] location = new int[2];
     // TODO: Add more features
 
+    /*
+     * setItem
+     * Purpose: Sets the item field
+     * Parameters: (Media) newItem
+     * Returns: Nothing (sets a value)
+     */
     public void setItem(Media newItem) {item = newItem;}
+
+    /*
+     * getItem
+     * Purpose: Gets value in item field
+     * Parameters: None
+     * Returns: The value in item
+     */
     public Media getItem() {return item;}
-    public void setUserRating(double newRating) {userRating = newRating;}
-    public double getUserRating() {return userRating;}
-    public void setUserReview(String newReview) {userReview = newReview;}
-    public String getUserReview() {return userReview;}
-    public void setHasSeen(boolean seen) {hasSeen = seen;}
-    public boolean getHasSeen() {return hasSeen;}
-    public void setEntryDescription(String newDescription) {entryDescription = newDescription;}
-    public String getEntryDescription() {return entryDescription;}
     
+    /*
+     * setUserRating
+     * Purpose: Set the userRating field
+     * Parameters: (double) newRating
+     * Returns: Nothing (sets a value)
+     */
+    public void setUserRating(double newRating) {userRating = newRating;}
+
+    /*
+     * getUserRating
+     * Purpose: Gets the value in userRating
+     * Parameters: None
+     * Returns: The value in userRating
+     */
+    public double getUserRating() {return userRating;}
+
+    /*
+     * setUserReview
+     * Purpose: Set the userReview field
+     * Parameters: (String) newReview
+     * Returns: Nothing (sets a value)
+     */
+    public void setUserReview(String newReview) {userReview = newReview;}
+
+    /*
+     * getUserReview
+     * Purpose: Gets the value in userReview
+     * Parameters: None
+     * Returns: The value in userReview
+     */
+    public String getUserReview() {return userReview;}
+
+    /* 
+     * setHasSeen
+     * Purpose: Set the hasSeen field
+     * Parameters: (boolean) seen
+     * Returns: Nothing (sets a value)
+     */
+    public void setHasSeen(boolean seen) {hasSeen = seen;}
+
+    /*
+     * getHasSeen
+     * Purpose: Gets the value in hasSeen
+     * Parameters: None
+     * Returns: The value in hasSeen
+     */
+    public boolean getHasSeen() {return hasSeen;}
+
+    /*
+     * setEntryDescription
+     * Purpose: Set the entryDescription field
+     * Parameters: (String) newDescription
+     * Returns: Nothing (sets a value)
+     */
+    public void setEntryDescription(String newDescription) {entryDescription = newDescription;}
+
+    /*
+     * getEntryDescription
+     * Purpose: Gets the value in entryDescription 
+     * Parameters: None
+     * Returns: The value in entryDescription
+     */
+    public String getEntryDescription() {return entryDescription;}
+
+    /*
+     * setLocation
+     * Purpose: Set the location field
+     * Parameters: (int[]) newLocation
+     * Returns: Nothing (sets a value)
+     */
+    public void setLocation(int[] newLocation) {location = newLocation;}
+
+    /*
+     * getLocation
+     * Purpose: Get the value in location 
+     * Parameters: None
+     * Returns The value in location
+     */
+    public int[] getLocation() {return location;}
 }
